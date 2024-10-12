a1224237f9c7c429c6e742b144b2ccb123a5014a
Tim Cares <peep-kestrel-0t@icloud.com>
2024-10-12 23:46:30 +0200

    added docs

--- src/datamodules/multi_data_module.py
+++ src/datamodules/multi_data_module.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Dict, Union, Iterable
+import torch
+import numpy as np
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader 
 from torch.utils.data.dataset import ConcatDataset
@@ -13,6 +15,19 @@ class MultiDataModule(LightningDataModule):
         shuffle:bool=True,
         drop_last:bool=True,
     ):
+        r"""
+        A datamodule that combines multiple datamodules.
+        The train/val/test datasets of the datamodules are treated as a single
+        train/val/test dataset.
+        
+        Args:
+            datamodules (List[LightningDataModule]): The datamodules to combine.
+            batch_size (int): The batch size to use for the combined datasets.
+            num_workers (int): The number of workers to use for the dataloaders.
+            shuffle (bool, optional): Whether to shuffle the training dataset items. Defaults to True.
+            drop_last (bool, optional): Whether to drop the last (incomplete) batch
+                from the train dataset, if is is < batch_size. Defaults to True.
+        """
         super().__init__()
         self.datamodules = datamodules
         self.batch_size = batch_size
@@ -25,10 +40,20 @@ class MultiDataModule(LightningDataModule):
         self.test_dataset = None
 
     def prepare_data(self):
+        """
+        Prepare the data for all datamodules.
+        """        
         for datamodule in self.datamodules:
             datamodule.prepare_data()
 
-    def setup(self, stage=None):
+    def setup(self, stage:str=None):
+        """Setup the data for all datamodules. If a datamodule has no dataset for a stage,
+        it will be ignored.
+
+        Args:
+            stage (str, optional): For which stage(s) to setup the data. Defaults to None, meaning all stages.
+                Passed to the "setup" method of the datamodules.
+        """        
         train_datasets = []
         val_datasets = []
         test_datasets = []
@@ -82,7 +107,15 @@ class MultiDataModule(LightningDataModule):
                           shuffle=False,
                           drop_last=False,)
     
-    def collater(self, samples):
+    def collater(self, samples:List[Dict[str, Union[torch.Tensor, np.ndarray, Iterable]]]) -> Dict[str, torch.Tensor]:
+        """Batches a list of samples into a single batch.
+
+        Args:
+            samples (List[Dict[str, Union[torch.Tensor, np.ndarray, Iterable]]]): The samples/items to batch.
+
+        Returns:
+            Dict[str, torch.Tensor]: The batched items.
+        """        
         return data_utils.collater(samples)
 
     def teardown(self, stage: str) -> None:
