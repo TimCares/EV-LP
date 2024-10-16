c112857cb9cd4995acfc6865184f6f322b09551d
Tim Cares <peep-kestrel-0t@icloud.com>
2024-10-16 20:16:54 +0200

    fixed error in imagenet download

--- src/datasets_/download/download_imagenet.py
+++ src/datasets_/download/download_imagenet.py
@@ -15,7 +15,8 @@ def main():
     os.makedirs(path_to_train_split, exist_ok=True)
     os.makedirs(path_to_val_split, exist_ok=True)
     
-    snapshot_download(repo_id="ILSVRC/imagenet-1k", repo_type="dataset", allow_patterns=["train_images_*", "val_images_*"], local_dir=path_to_data)
+    snapshot_download(repo_id="ILSVRC/imagenet-1k", repo_type="dataset",
+                      allow_patterns=["data/train_images_*", "data/val_images_*"], local_dir=path_to_data)
 
     pattern = os.path.join(path_to_data, 'train_images*.tar.gz')
     train_archives = glob.glob(pattern)
@@ -30,3 +31,6 @@ def main():
     with tarfile.open(val_archive, 'r') as tar:
         tar.extractall(path_to_val_split)
     os.remove(val_archive)
+
+if __name__ == "__main__":
+    main()
