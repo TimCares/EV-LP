0a90cf7f24c33fa84b2116fe4d17463108748bdb
Tim Cares <peep-kestrel-0t@icloud.com>
2024-10-16 20:34:30 +0200

    fix for imagenet download

--- src/datasets_/download/download_imagenet.py
+++ src/datasets_/download/download_imagenet.py
@@ -18,7 +18,7 @@ def main():
     snapshot_download(repo_id="ILSVRC/imagenet-1k", repo_type="dataset",
                       allow_patterns=["data/train_images_*", "data/val_images_*"], local_dir=path_to_data)
 
-    pattern = os.path.join(path_to_data, 'train_images*.tar.gz')
+    pattern = os.path.join(path_to_data, 'data', 'train_images*.tar.gz')
     train_archives = glob.glob(pattern)
     print(f"Extracting {len(train_archives)} train archives")
     for file in train_archives:
@@ -26,7 +26,7 @@ def main():
             tar.extractall(path_to_train_split)
         os.remove(file)
 
-    val_archive = os.path.join(path_to_data, 'val_images.tar.gz')
+    val_archive = os.path.join(path_to_data, 'data', 'val_images.tar.gz')
     print("Extracting val archive")
     with tarfile.open(val_archive, 'r') as tar:
         tar.extractall(path_to_val_split)
